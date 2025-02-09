04b7501c036934d146dac48606f67fdd23ae0318
huhn511 <huhn.dev@gmail.com>
2025-02-09 14:19:11 +0100

    fix browser only error

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import Map from '../components/Map/index';
 import Layout from '@theme/Layout';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
-const HomePage = () => (
-  <Layout>
-    <h1>Fruit Trees and Areas</h1>
-    <Map />
-  </Layout>
-);
-
-export default HomePage;
+export default function HomePage() {
+  return (
+    <Layout>
+      <h1>Fruit Trees and Areas</h1>
+      <BrowserOnly fallback={<div>Lade Map …</div>}>
+        {() => {
+          const Map = require('../components/Map/index').default;
+          return <Map />;
+        }}
+      </BrowserOnly>
+    </Layout>
+  );
+}
